1fcab7f30a5ab2484118d47a9edf6ec938840324
Zandarck <oliver.d.bray@protonmail.com>
2020-12-10 18:17:22 +0000

    Ground work done.

--- FileFinder.py
+++ FileFinder.py
@@ -1,13 +1,10 @@
-import os
-from pathlib import Path
+# import os
+import glob  # glob is used for searching
+# from pathlib import Path
 
-search = input("What would you like to find:\n> ")
-results = []
-for file_name in os.path.listdir:
-    if search in file_name:
-        results.append(file_name)
-
-print(results)
-
-
-# USE GLOB
+file_search = input("What would you like to find:\n> ")
+file_type = input("What type of file are you looking for:\n> ")
+file_location = input("If you know the rough location let me know:\n> ")
+string = str(file_location + file_search + file_type)
+result = glob.glob(f"{string}", recursive=False)
+print(result)
