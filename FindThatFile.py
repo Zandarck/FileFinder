424ce970da3d2f94b53394ca4995f2d2d54a60b1
Zandarck <oliver.d.bray@protonmail.com>
2020-12-14 20:54:11 +0000

    More changes, need to run through the resources notepads

--- FindThatFile.py
+++ FindThatFile.py
@@ -2,48 +2,87 @@
 import glob  # glob is used for searching
 # from pathlib import Path
 
-type_file = "nul"
+file_type = "nul"
+file_name = "nul"
+file_location = "nul"
 
 
 def start_find():
-    global type_file
+    global file_type
     start = int(input("""What type of file do you want to find?\n
         1 - Document, 2 - Video, 3 - Audio\n
         4 - Photo, 5 - Self-defined\n> """))
     if start == 1:
         print("1\n\n\n")
-        type_file = "doc"
+        file_type = "doc"
+        choice_find()
 
     elif start == 2:
         print("2\n\n\n")
-        type_file = "vid"
+        file_type = "vid"
+        choice_find()
 
     elif start == 3:
         print("3\n\n\n")
-        type_file = "aud"
+        file_type = "aud"
+        choice_find()
 
     elif start == 4:
         print("4\n\n\n")
-        type_file = "imd"
+        file_type = "img"
+        choice_find()
 
     elif start == 5:
         print("5\n\n\n")
-        type_file = "usr"
+        file_type = "usr"
+        choice_find()
 
     else:
         print("Please provide a number.\n\n\n")
         start_find()
 
 
-def file_find():
-    file_search = input("What would you like to find?:\n> ")
-    file_type = input("What type of file are you looking for?:\n> ")
-    file_location = input("Where do you want to search?:\n> ")
-    string = str(file_location + file_search + "." + file_type)
+def choice_find():
+    global file_name
+    global file_type
+    global file_location
+    if file_type == "doc":
+        print("Searching for documents")
+        file_name = input("What would you like to find?:\n> ")
+        file_location = input("Where do you want to search?:\n> ")
+        user_file_find(file_name, file_type, file_location)
+
+    elif file_type == "vid":
+        print("Searching for videos")
+        file_name = input("What would you like to find?:\n> ")
+        file_location = input("Where do you want to search?:\n> ")
+        user_file_find(file_name, file_type, file_location)
+
+    elif file_type == "aud":
+        print("Searching for audio tracks")
+        file_name = input("What would you like to find?:\n> ")
+        file_location = input("Where do you want to search?:\n> ")
+        user_file_find(file_name, file_type, file_location)
+
+    elif file_type == "img":
+        print("Searching for images")
+        file_name = input("What would you like to find?:\n> ")
+        file_location = input("Where do you want to search?:\n> ")
+        user_file_find(file_name, file_type, file_location)
+
+    elif file_type == "usr":
+        print("Searching for user defined extention")
+        file_name = input("What would you like to find?:\n> ")
+        file_type = input("What type of file are you looking for?:\n> ")
+        file_location = input("Where do you want to search?:\n> ")
+        user_file_find(file_name, file_type, file_location)
+
+
+def user_file_find(name, type, local):
+    string = str(file_location + file_name + "." + file_type)
     result = glob.glob(f"{string}", recursive=True)
     print(result)
-
-
+    print(string)
 
 
 start_find()
