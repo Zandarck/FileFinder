448330cc85549f204dbff05ba1cf93ca17954567
Zandarck <oliver.d.bray@protonmail.com>
2020-12-15 17:24:37 +0000

    Ver.1.0!
    Basic search available! Got to work on the other features.

--- FindThatFile.py
+++ FindThatFile.py
@@ -1,88 +1,106 @@
-# import os
 import glob  # glob is used for searching
-# from pathlib import Path
-
-file_type = "nul"
-file_name = "nul"
-file_location = "nul"
 
 
 def start_find():
-    global file_type
     start = int(input("""What type of file do you want to find?\n
         1 - Document, 2 - Video, 3 - Audio\n
         4 - Photo, 5 - Self-defined\n> """))
     if start == 1:
-        print("1\n\n\n")
-        file_type = "doc"
-        choice_find()
+        f_type = "doc"
+        print(f_type)
+        choice_find(f_type)
 
     elif start == 2:
-        print("2\n\n\n")
-        file_type = "vid"
-        choice_find()
+        f_type = "vid"
+        print(f_type)
+        choice_find(f_type)
 
     elif start == 3:
-        print("3\n\n\n")
-        file_type = "aud"
-        choice_find()
+        f_type = "aud"
+        print(f_type)
+        choice_find(f_type)
 
     elif start == 4:
-        print("4\n\n\n")
-        file_type = "img"
-        choice_find()
+        f_type = "img"
+        print(f_type)
+        choice_find(f_type)
 
     elif start == 5:
-        print("5\n\n\n")
-        file_type = "usr"
-        choice_find()
+        f_type = "usr"
+        print(f_type)
+        choice_find(f_type)
 
     else:
         print("Please provide a number.\n\n\n")
         start_find()
 
 
-def choice_find():
-    global file_name
-    global file_type
-    global file_location
-    if file_type == "doc":
-        print("Searching for documents")
-        file_name = input("What would you like to find?:\n> ")
-        file_location = input("Where do you want to search?:\n> ")
-        user_file_find(file_name, file_type, file_location)
+def choice_find(f_type):
+    if f_type == "doc":
+        print("Searching for documents:")
+        ext_list = open("Resources/documents.txt", "r")
+        list_lines = ext_list.read().splitlines()
+        f_name = input("What would you like to find?:\n> ")
+        f_loc = input("Where do you want to search?:\n> ")
+        name_type = [f_name + "." + line for line in list_lines]
+        ext_list.close()
+        results = []
+        for item in name_type:
+            results.append(glob.glob(f_loc + item, recursive=True))
+        fil_results = list(filter(None, results))
+        print(fil_results)
 
-    elif file_type == "vid":
-        print("Searching for videos")
-        file_name = input("What would you like to find?:\n> ")
-        file_location = input("Where do you want to search?:\n> ")
-        user_file_find(file_name, file_type, file_location)
+    elif f_type == "vid":
+        print("Searching for videos:")
+        ext_list = open("Resources/videos.txt", "r")
+        list_lines = ext_list.read().splitlines()
+        f_name = input("What would you like to find?:\n> ")
+        f_loc = input("Where do you want to search?:\n> ")
+        name_type = [f_name + "." + line for line in list_lines]
+        ext_list.close()
+        results = []
+        for item in name_type:
+            results.append(glob.glob(f_loc + item, recursive=True))
+        fil_results = list(filter(None, results))
+        print(fil_results)
 
-    elif file_type == "aud":
-        print("Searching for audio tracks")
-        file_name = input("What would you like to find?:\n> ")
-        file_location = input("Where do you want to search?:\n> ")
-        user_file_find(file_name, file_type, file_location)
+    elif f_type == "aud":
+        print("Searching for audio tracks:")
+        ext_list = open("Resources/audio.txt", "r")
+        list_lines = ext_list.read().splitlines()
+        f_name = input("What would you like to find?:\n> ")
+        f_loc = input("Where do you want to search?:\n> ")
+        name_type = [f_name + "." + line for line in list_lines]
+        ext_list.close()
+        results = []
+        for item in name_type:
+            results.append(glob.glob(f_loc + item, recursive=True))
+        fil_results = list(filter(None, results))
+        print(fil_results)
 
-    elif file_type == "img":
-        print("Searching for images")
-        file_name = input("What would you like to find?:\n> ")
-        file_location = input("Where do you want to search?:\n> ")
-        user_file_find(file_name, file_type, file_location)
+    elif f_type == "img":
+        print("Searching for images:")
+        ext_list = open("Resources/images.txt", "r")
+        list_lines = ext_list.read().splitlines()
+        f_name = input("What would you like to find?:\n> ")
+        f_loc = input("Where do you want to search?:\n> ")
+        name_type = [f_name + "." + line for line in list_lines]
+        ext_list.close()
+        results = []
+        for item in name_type:
+            results.append(glob.glob(f_loc + item, recursive=True))
+        fil_results = list(filter(None, results))
+        print(fil_results)
 
-    elif file_type == "usr":
-        print("Searching for user defined extention")
-        file_name = input("What would you like to find?:\n> ")
-        file_type = input("What type of file are you looking for?:\n> ")
-        file_location = input("Where do you want to search?:\n> ")
-        user_file_find(file_name, file_type, file_location)
-
-
-def user_file_find(name, type, local):
-    string = str(file_location + file_name + "." + file_type)
-    result = glob.glob(f"{string}", recursive=True)
-    print(result)
-    print(string)
+    elif f_type == "usr":
+        print("Searching for user defined extention:")
+        f_name = input("What would you like to find?:\n> ")
+        f_type = input("What type of file are you looking for?:\n> ")
+        f_loc = input("Where do you want to search?:\n> ")
+        results = []
+        results.append(glob.glob(f_loc + f_name + f_type, recursive=True))
+        fil_results = list(filter(None, results))
+        print(fil_results)
 
 
 start_find()
--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # Find That File!
- A Python based file finder
+ A Python based file finder!
+
+ Want to find a file? This may help! Uses the text files in the _Resources_ folder to create a list with the file name to add well known file extentions and hopefully it will find that file. WIP.
+
+
+ #### Usage:
+ Load up a terminal/command prompt in the main directory and type:
+
+ ```python FindThatFile.py```
+
+From here follow the prompts (file type, file name and location) and the program will give you the locations of any matching files.
+
+
 
 ###### To add:
 - [ ] Search by:
-  - [ ] Type
+  - [ ] ~~Type~~
   - [ ] Edit/creation date
   - [ ] To/From date
 -[ ] Search style:
@@ -11,8 +23,10 @@
     - [ ] Strict
 - [ ] Additional:
   - [ ] Supply the directory
-  - [ ] Add file paths to a .txt
+  - [ ] ~~Add file paths to a document~~
+  - [ ] Recursive search (subdirectories)
   - [ ] Open directory
   - [ ] Open Explorer showing the files
 - [ ] Waaaaaaaay in the future:
-  - [ ] Different OS compatability
+  - [ ] Different OS compatability - will need to test as it maybe this by default...
+  - [ ] GUI
